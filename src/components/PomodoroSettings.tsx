5e94c5d1b9a7093a9818de9992354125a9921c70
Mayank Gupta <gupta.mayank2003@gmail.com>
2024-07-24 23:12:25 +0530

    optimized pomodor timer

--- src/components/PomodoroSettings.tsx
+++ src/components/PomodoroSettings.tsx
@@ -1,72 +1,82 @@
 import { FC, ChangeEvent } from "react";
-import { Settings } from "lucide-react"
-import { Button } from "./ui/button"
-import { Dialog, DialogContent, DialogDescription, DialogHeader, DialogTitle, DialogTrigger } from "./ui/dialog"
-import { Input } from "./ui/input"
-import { Label } from "./ui/label"
+import { Settings } from "lucide-react";
+import { Button } from "./ui/button";
+import {
+  Dialog,
+  DialogContent,
+  DialogDescription,
+  DialogHeader,
+  DialogTitle,
+  DialogTrigger,
+} from "./ui/dialog";
+import { Input } from "./ui/input";
+import { Label } from "./ui/label";
 interface Timers {
-    workTime: number;
-    breakTime: number;
+  workTime: number;
+  breakTime: number;
 }
 
 interface PomodoroSettingsProps {
-    timers: Timers;
-    setTimers: React.Dispatch<React.SetStateAction<{
-        workTime: number;
-        breakTime: number;
-    }>>
+  timers: Timers;
+  setTimers: React.Dispatch<
+    React.SetStateAction<{
+      workTime: number;
+      breakTime: number;
+    }>
+  >;
 }
 
 const PomodoroSettings: FC<PomodoroSettingsProps> = ({ timers, setTimers }) => {
+  const handleTimeChange = (e: ChangeEvent<HTMLInputElement>) => {
+    const { name, value } = e.target;
+    setTimers((prevTimers) => ({
+      ...prevTimers,
+      [name]: parseInt(value),
+    }));
+  };
 
-    const handleTimeChange = (e: ChangeEvent<HTMLInputElement>) => {
-        const { name, value } = e.target;
-        setTimers((prevTimers) => ({
-            ...prevTimers,
-            [name]: parseInt(value),
-        }));
-    }
+  return (
+    <Dialog>
+      <DialogTrigger asChild>
+        <Button
+          className="absolute top-2 right-2"
+          size="icon"
+          type="button"
+          variant="link"
+        >
+          <Settings />
+        </Button>
+      </DialogTrigger>
+      <DialogContent className="sm:max-w-[425px]">
+        <DialogHeader>
+          <DialogTitle>Edit Timer</DialogTitle>
+          <DialogDescription>
+            Make changes to your timer here. Click save when you're done.
+          </DialogDescription>
+        </DialogHeader>
+        <Label htmlFor="workTime">Work Time</Label>
+        <Input
+          type="number"
+          id="workTime"
+          name="workTime"
+          min={1}
+          max={60}
+          value={timers.workTime}
+          onChange={handleTimeChange}
+        />
+        <Label htmlFor="breakTime">Break Time</Label>
+        <Input
+          type="number"
+          id="breakTime"
+          name="breakTime"
+          min={1}
+          max={60}
+          value={timers.breakTime}
+          onChange={handleTimeChange}
+        />
+      </DialogContent>
+    </Dialog>
+  );
+};
 
-    return (
-        <Dialog>
-            <DialogTrigger asChild>
-                <Button
-                    className="absolute top-2 right-2"
-                    size="icon"
-                    type="button"
-                    variant="link">
-                    < Settings />
-                </Button>
-            </DialogTrigger>
-            <DialogContent className="sm:max-w-[425px]">
-                <DialogHeader>
-                    <DialogTitle>Edit Timer</DialogTitle>
-                    <DialogDescription>
-                        Make changes to your timer here. Click save when you're done.
-                    </DialogDescription>
-                </DialogHeader>
-                <Label htmlFor="workTime">Work Time</Label>
-                <Input
-                    type="number"
-                    id="workTime"
-                    name="workTime"
-                    min={1}
-                    max={60}
-                    value={timers.workTime}
-                    onChange={handleTimeChange} />
-                <Label htmlFor="breakTime">Break Time</Label>
-                <Input
-                    type="number"
-                    id="breakTime"
-                    name="breakTime"
-                    min={1}
-                    max={60}
-                    value={timers.breakTime}
-                    onChange={handleTimeChange} />
-            </DialogContent>
-        </Dialog>
-    )
-}
-
-export default PomodoroSettings
-
+export default PomodoroSettings;
--- src/components/widgets/PomodoroTimer.tsx
+++ src/components/widgets/PomodoroTimer.tsx
@@ -1,71 +1,87 @@
-import { useEffect, useState } from "react";
+import { useEffect, useMemo, useState, useCallback } from "react";
 import Stopwatch from "../Stopwatch";
 import { Button } from "../ui/button";
 import {
-    Card,
-    CardContent,
-    CardDescription,
-    CardFooter,
-    CardHeader,
-    CardTitle,
+  Card,
+  CardContent,
+  CardDescription,
+  CardFooter,
+  CardHeader,
+  CardTitle,
 } from "../ui/card";
 import PomodoroSettings from "../PomodoroSettings";
 import Analog from "../Analog";
 
 const PomodoroTimer = () => {
-    const [time, setTime] = useState(new Date());
-    const [isWorkTime, setIsWorkTime] = useState(true);
-    const [isRunningClock, setIsRunningClock] = useState(false);
-    const [timers, setTimers] = useState(() => {
-        const savedTimers = sessionStorage.getItem("timer");
-        return savedTimers
-            ? JSON.parse(savedTimers)
-            : { workTime: 25, breakTime: 5 };
-    });
-    const [isRunning, setIsRunning] = useState(false);
+  const [time, setTime] = useState(new Date());
+  const [isWorkTime, setIsWorkTime] = useState(true);
+  const [isRunningClock, setIsRunningClock] = useState(false);
 
-    useEffect(() => {
-        sessionStorage.setItem("timer", JSON.stringify(timers));
-    }, [timers]);
+  const [timers, setTimers] = useState(() => {
+    const savedTimers = sessionStorage.getItem("timer");
+    return savedTimers
+      ? JSON.parse(savedTimers)
+      : { workTime: 25, breakTime: 5 };
+  });
 
-    const handleStart = () => {
-        setIsRunningClock((prev) => !prev);
-        setTime(new Date());
-        setIsRunning((prev) => !prev);
-    };
+  const [isRunning, setIsRunning] = useState(false);
 
-    return (
-        <Card className="w-80 h-96 rounded-2xl bg-[#bfc1b5] relative ">
-            <CardHeader className="mt-8">
-                <CardTitle className="text-center">
-                    {isWorkTime ? "Pomodoro" : "Break"}
-                </CardTitle>
-                <CardDescription className="hidden">A Pomodoro Timer</CardDescription>
-            </CardHeader>
-            <CardContent className="flex flex-col justify-center items-center gap-6">
-                <PomodoroSettings timers={timers} setTimers={setTimers} />
-                <Analog isRunningClock={isRunningClock} setTime={setTime} time={time} />
-                <Stopwatch
-                    minutes={timers.workTime}
-                    breakTime={timers.breakTime}
-                    isRunning={isRunning}
-                    setIsWorkTime={setIsWorkTime}
-                    isWorkTime={isWorkTime}
-                />
-            </CardContent>
-            <CardFooter className="flex items-center justify-center ">
-                <Button
-                    onClick={handleStart}
-                    variant="outline"
-                    className="shadow-lg w-20 h-10 bg-[#D0D0D0] border-none"
-                >
-                    <p className="text-lg text-[#2A2A2A]">
-                        {isRunning ? "Pause" : "Start"}
-                    </p>
-                </Button>
-            </CardFooter>
-        </Card>
-    );
+  useEffect(() => {
+    sessionStorage.setItem("timer", JSON.stringify(timers));
+  }, [timers]);
+
+  const handleStart = useCallback(() => {
+    setIsRunningClock((prev) => !prev);
+    setTime(new Date());
+    setIsRunning((prev) => !prev);
+  }, []);
+
+  const cardHeader = useMemo(
+    () => (
+      <CardHeader className="mt-8">
+        <CardTitle className="text-center">
+          {isWorkTime ? "Pomodoro" : "Break"}
+        </CardTitle>
+        <CardDescription className="hidden">A Pomodoro Timer</CardDescription>
+      </CardHeader>
+    ),
+    [isWorkTime]
+  );
+
+  const cardFooter = useMemo(
+    () => (
+      <CardFooter className="flex items-center justify-center">
+        <Button
+          onClick={handleStart}
+          variant="outline"
+          className="shadow-lg w-20 h-10 bg-[#D0D0D0] border-none"
+        >
+          <p className="text-lg text-[#2A2A2A]">
+            {isRunning ? "Pause" : "Start"}
+          </p>
+        </Button>
+      </CardFooter>
+    ),
+    [isRunning, handleStart]
+  );
+
+  return (
+    <Card className="w-80 h-96 rounded-2xl bg-[#bfc1b5] relative">
+      {cardHeader}
+      <CardContent className="flex flex-col justify-center items-center gap-6">
+        <PomodoroSettings timers={timers} setTimers={setTimers} />
+        <Analog isRunningClock={isRunningClock} setTime={setTime} time={time} />
+        <Stopwatch
+          minutes={timers.workTime}
+          breakTime={timers.breakTime}
+          isRunning={isRunning}
+          setIsWorkTime={setIsWorkTime}
+          isWorkTime={isWorkTime}
+        />
+      </CardContent>
+      {cardFooter}
+    </Card>
+  );
 };
 
 export default PomodoroTimer;
