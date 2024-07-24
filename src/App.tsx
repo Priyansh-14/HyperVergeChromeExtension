6c2ca04112f675083a909fb8b558f0a3304fbc83
Mayank Gupta <gupta.mayank2003@gmail.com>
2024-07-24 09:32:41 +0530

    initialized shadcn

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,22 @@
-import { useState } from 'react'
+import { useState } from "react";
+import { Button } from "./components/ui/button";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [count, setCount] = useState(0);
 
   return (
-    <div className='bg-[#2a2a2a] h-screen'>
-      <h1>Digital Notice Board</h1>
+    <div className="bg-[#2a2a2a] h-screen">
+      <h1 className="text-white text-2xl mx-auto">Digital Notice Board</h1>
       <div className="card text-green-700">
-        <button onClick={() => setCount((count) => count + 1)}>
+        <Button
+          className="text-white"
+          onClick={() => setCount((count) => count + 1)}
+        >
           count is {count}
-        </button>
+        </Button>
       </div>
-
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
